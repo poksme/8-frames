40f498806bf7050c01c96c599ded35127f77652c
Bertrand Boustany <bertrand@boustany.me>
2016-06-27 19:45:22 +0900

    new architecture

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,35 +1,16 @@
-// AppComponent Class definition
-
-import { Component, OnInit } from '@angular/core'; // Import the @Component decorator
-import { FighterDetailComponent } from './fighter-detail.component'
-import { Fighter } from './fighter';
+import { Component } from '@angular/core';
 import { FighterService } from './fighter.service';
-
+import { FightersComponent } from './fighters.component';
 @Component({
-    selector: 'my-app',
-    templateUrl:'./templates/app.html',
-    styleUrls: ['./stylesheets/app.css'],
-    directives: [FighterDetailComponent],
-    providers: [FighterService]
-}) export class AppComponent implements OnInit {
-    title = 'Street Fighters';
-    fighters: Fighter[];
-    selectedFighter : Fighter;
-
-    constructor(private fighterService: FighterService) {
-    }
-
-    initFighters() {
-        this.fighterService.fetchFighters().then(function (fighters) {
-             this.fighters = fighters
-        }.bind(this));
-    }
-
-    onSelect(fighter: Fighter) {
-        this.selectedFighter = fighter;
-    }
-
-    ngOnInit() {
-        this.initFighters();
-    }
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <fighters></fighters>
+  `,
+  directives: [FightersComponent],
+  providers: [
+    FighterService
+  ]
+}) export class AppComponent {
+  title = 'Street Fighters App';
 }
---
+++ app/fighters.component.ts
@@ -0,0 +1,35 @@
+// AppComponent Class definition
+
+import { Component, OnInit } from '@angular/core'; // Import the @Component decorator
+import { FighterDetailComponent } from './fighter-detail.component'
+import { Fighter } from './fighter';
+import { FighterService } from './fighter.service';
+
+@Component({
+    selector: 'fighters',
+    templateUrl:'./templates/fighters.html',
+    styleUrls: ['./stylesheets/fighters.css'],
+    directives: [FighterDetailComponent],
+    providers: [FighterService]
+}) export class FightersComponent implements OnInit {
+    title = 'Available Fighters';
+    fighters: Fighter[];
+    selectedFighter : Fighter;
+
+    constructor(private fighterService: FighterService) {
+    }
+
+    initFighters() {
+        this.fighterService.fetchFighters().then(function (fighters) {
+             this.fighters = fighters
+        }.bind(this));
+    }
+
+    onSelect(fighter: Fighter) {
+        this.selectedFighter = fighter;
+    }
+
+    ngOnInit() {
+        this.initFighters();
+    }
+}
