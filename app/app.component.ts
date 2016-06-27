f0c96e5c04f503af8c5657afcf39705de948c487
Bertrand Boustany <bertrand@boustany.me>
2016-06-27 17:33:12 +0900

    The fighter editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,34 @@
 
 import { Component } from '@angular/core'; // Import the @Component decorator
 
+export class Fighter {
+    id: number;
+    name: string;
+}
+
 @Component({ // Decorator
    selector: 'my-app', // CSS Selector where instance of AppComponent will be created
-   template: '<h1>My first Angular 2 App</h1>' // What HTML template to render
+   template: `
+<h1>{{title}}</h1>
+<h2>{{fighter.name}} details!</h2>
+<div>
+    <label>id: </label>{{fighter.id}}
+</div>
+<div>
+    <label>name: </label>
+    <input [(ngModel)]="fighter.name" placeholder="name">
+</div>
+  ` // What HTML template to render
 })
 
 //  @Component     |   This invoked decorator 
 //  applies to     |        applies to
 // AppComponent   \_/       this class
 
-export class AppComponent { } // Control appearance and behaviour of the component
+export class AppComponent {
+    title = 'Street Fighters';
+    fighter : Fighter = {
+        id: 1,
+        name: 'Ryu'
+    }
+ } // Control appearance and behaviour of the component
